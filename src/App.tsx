3efe5fa3565de76d8bef713725b1de626c9396aa
Dallin Romney <dallinromney@gmail.com>
2023-08-11 02:17:14 -0400

    initial go

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import ExamplePage from '@dromney/react-gear-gen/src/examples/ExamplePage';
+function App() {
+  return (
+    <div className="container">
+      <ExamplePage />
+    </div>
+  );
+}
+
+export default App;
