981f2d9b9e45a789f6380d2c05b241a7f1d95939
Dallin Romney <dallinromney@gmail.com>
2023-08-11 06:35:04 -0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React, { useEffect, useState } from 'react';
 import './App.css';
-import { ExamplePage, MouseGearSetViewer, PositionedGearSetViewer } from '@dromney/react-gear-gen';
-import { Gear, GearSet, ExampleGears } from '@dromney/gear-gen';
+import { ExamplePage } from '@dromney/react-gear-gen';
 
 function App() {
-  const [gearSet1, setGearSet1] = useState<GearSet>()
-  useEffect(() => {
-    setGearSet1(new GearSet(ExampleGears()))
-  }, [])
-
   return (
     <div className="container">
       <ExamplePage />
