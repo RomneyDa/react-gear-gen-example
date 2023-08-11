53828a6aed7b562c469b8797b5ba0f236565767f
Dallin Romney <dallinromney@gmail.com>
2023-08-11 06:31:23 -0400

    working! - fix alignment issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
-// import { ExamplePage } from '../react-gear-gen';
-import { ExamplePage } from '../../react-gear-gen/dist/examples';
+import { ExamplePage, MouseGearSetViewer, PositionedGearSetViewer } from '@dromney/react-gear-gen';
+import { Gear, GearSet, ExampleGears } from '@dromney/gear-gen';
+
 function App() {
+  const [gearSet1, setGearSet1] = useState<GearSet>()
+  useEffect(() => {
+    setGearSet1(new GearSet(ExampleGears()))
+  }, [])
+
   return (
     <div className="container">
       <ExamplePage />
