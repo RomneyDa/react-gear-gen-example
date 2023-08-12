8a2746e60bff6da4ebf18009c0b651e52aff98d0
Dallin Romney <dallinromney@gmail.com>
2023-08-12 09:49:01 -0400

    use is mobile example page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { ExamplePage } from '@dromney/react-gear-gen';
 import '@dromney/gear-gen/dist/styles/dark.css'
+import { isMobile } from 'react-device-detect';
 
 function App() {
   return (
     <div className="container">
-      <ExamplePage />
+      <ExamplePage isMobile={isMobile} />
     </div>
   );
 }
